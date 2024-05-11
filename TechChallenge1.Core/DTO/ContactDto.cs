b59fd0957d9d93655ff1370117aedfb1605425d0
ricardocza <ricardovcza@gmail.com>
2024-05-11 11:41:15 -0300

    Adicionada validações na adição do ContactDto

--- TechChallenge1.Core/DTO/ContactDto.cs
+++ TechChallenge1.Core/DTO/ContactDto.cs
@@ -1,4 +1,5 @@
 ﻿
+using System.ComponentModel.DataAnnotations;
 using TechChallenge1.Core.Models;
 
 namespace TechChallenge1.Core.DTO
@@ -6,11 +7,22 @@ namespace TechChallenge1.Core.DTO
     public class ContactDto
     {
         public Guid Id { get; set; }
-        public string Name { get; set; }
-        public string Phone { get; set; }
-        public string Email { get; set; }
-        public int StateId { get; set; }
-        public StateDto? State { get; set; }
 
+        [Required(ErrorMessage ="O nome é obrigatório")]
+        [StringLength(50, ErrorMessage = "O nome deve ter no máximo 50 caracteres")]
+        public string Name { get; set; }
+
+        [Required(ErrorMessage = "O telefone é obrigatório")]
+        [StringLength(11, MinimumLength = 10,  ErrorMessage = "O telefone deve ter entre 10 e 11 números")]
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "O email é obrigatório")]
+        [EmailAddress(ErrorMessage = "O email é inválido")]
+        public string Email { get; set; }
+
+        [Required(ErrorMessage = "O estado é obrigatório")]
+        public string StateName { get; set; } = "";
+
+        public int StateId { get; set; }
     }
 }
